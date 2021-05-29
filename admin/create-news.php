1103320faf3f9b594145c9f50bef9d5c900275f9
chalanadulajdilshan <chalanadulaj98@gmail.com>
2021-05-29 16:12:41 +0530

    #43 Admin desing

---
+++ admin/create-news.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8" />
+        <title>Basic Elements | Minible - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesbrand" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+
+    <body>
+
+        <!-- <body data-layout="horizontal" data-topbar="colored"> -->
+
+        <!-- Begin page -->
+        <div id="layout-wrapper">
+
+
+            <?php include './top-header.php'; ?>
+            <!-- ========== Left Sidebar Start ========== -->
+            <?php include './navigation.php'; ?>
+            <!-- Left Sidebar End -->
+
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box d-flex align-items-center justify-content-between">
+                                    <h4 class="mb-0">Basic Elements</h4>
+
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Forms</a></li>
+                                            <li class="breadcrumb-item active">Basic Elements</li>
+                                        </ol>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end page title -->
+
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-body">
+
+                                        <h4 class="card-title">Add Leader Details.</h4> 
+
+                                        <div class="mb-3 row">
+                                            <label for="example-text-input" class="col-md-2 col-form-label">Name</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="text"   id="example-text-input">
+                                            </div>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="example-search-input" class="col-md-2 col-form-label">Position</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="text"  id="example-search-input">
+                                            </div>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="example-email-input" class="col-md-2 col-form-label">Image</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="file"  id="example-email-input">
+                                            </div>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="example-url-input" class="col-md-2 col-form-label">Facebook URL</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="url"   id="example-url-input">
+                                            </div>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="example-url-input" class="col-md-2 col-form-label">Twitter URL</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="url"   id="example-url-input">
+                                            </div>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="example-url-input" class="col-md-2 col-form-label">Instergrame URL</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="url"   id="example-url-input">
+                                            </div>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="example-url-input" class="col-md-2 col-form-label">Linkind URL</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="url"   id="example-url-input">
+                                            </div>
+                                        </div> 
+                                        <div class="row">
+                                            <div class="col-12" style="display: flex; justify-content: flex-end;margin-top: 15px;">
+                                                <button class="btn btn-primary " type="submit" >Submit form</button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+                         
+
+                        <!-- end row -->
+
+                    </div> <!-- container-fluid -->
+                </div> 
+                <?php include './footer.php'; ?>
+            </div> 
+        </div>
+        <!-- END layout-wrapper -->
+
+
+
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+        <script src="assets/libs/waypoints/lib/jquery.waypoints.min.js"></script>
+        <script src="assets/libs/jquery.counterup/jquery.counterup.min.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/app.js"></script>
+
+    </body>
+</html>
