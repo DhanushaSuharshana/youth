750423fa7a45c5321557b33be351f52b1f2e9db6
chalanadulajdilshan <chalanadulaj98@gmail.com>
2021-05-30 12:23:47 +0530

    #45 Add new desings admin panel

--- admin/navigation.php
+++ admin/navigation.php
@@ -48,6 +48,17 @@
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="uil-store"></i>
+                        <span>About us</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="create-course-type.php">About NYSC</a></li>
+                        <li><a href="create-courses.php">Add New Course</a></li>
+                        <li><a href="#">Manage Courses</a></li>  
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="uil-store"></i>
                         <span>Courses</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
@@ -55,7 +66,7 @@
                         <li><a href="create-courses.php">Add New Course</a></li>
                         <li><a href="#">Manage Courses</a></li>  
                     </ul>
-                </li    >
+                </li>
 
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
@@ -87,6 +98,25 @@
                         <li><a href="manage-devision.php">Manage Devision</a></li>
                     </ul>
                 </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="uil-envelope"></i>
+                        <span>Services</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="create-devision.php">Add Services</a></li>
+                        <li><a href="manage-devision.php">Manage Services</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="uil-envelope"></i>
+                        <span>Photo Album</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="create-devision.php">Add Photo Album</a></li>
+                    </ul>
+                </li>
 
 
                 <li>
@@ -100,163 +130,7 @@
                         <li><a href="manage-news.php">Benefit</a></li>
                         <li><a href="manage-news.php">Youth Club News</a></li>
                     </ul>
-                </li> 
-
-                <li>
-                    <a href="#" class="has-arrow waves-effect">
-
-                        <span>Photo Album</span>
-                    </a>
-
-                </li> 
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect"> 
-                        <span>Authentication</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="auth-login.html">Login</a></li>
-                        <li><a href="auth-register.html">Register</a></li>
-                        <li><a href="auth-recoverpw.html">Recover Password</a></li>
-                        <li><a href="auth-lock-screen.html">Lock Screen</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="uil-file-alt"></i>
-                        <span>Utility</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="pages-starter.html">Starter Page</a></li>
-                        <li><a href="pages-maintenance.html">Maintenance</a></li>
-                        <li><a href="pages-comingsoon.html">Coming Soon</a></li>
-                        <li><a href="pages-timeline.html">Timeline</a></li>
-                        <li><a href="pages-faqs.html">FAQs</a></li>
-                        <li><a href="pages-pricing.html">Pricing</a></li>
-                        <li><a href="pages-404.html">Error 404</a></li>
-                        <li><a href="pages-500.html">Error 500</a></li>
-                    </ul>
-                </li>
-
-                <li class="menu-title">Components</li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="uil-flask"></i>
-                        <span>UI Elements</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="ui-alerts.html">Alerts</a></li>
-                        <li><a href="ui-buttons.html">Buttons</a></li>
-                        <li><a href="ui-cards.html">Cards</a></li>
-                        <li><a href="ui-carousel.html">Carousel</a></li>
-                        <li><a href="ui-dropdowns.html">Dropdowns</a></li>
-                        <li><a href="ui-grid.html">Grid</a></li>
-                        <li><a href="ui-images.html">Images</a></li>
-                        <li><a href="ui-lightbox.html">Lightbox</a></li>
-                        <li><a href="ui-modals.html">Modals</a></li>
-                        <li><a href="ui-offcanvas.html">Offcanvas</a></li>
-                        <li><a href="ui-rangeslider.html">Range Slider</a></li>
-                        <li><a href="ui-session-timeout.html">Session Timeout</a></li>
-                        <li><a href="ui-progressbars.html">Progress Bars</a></li>
-                        <li><a href="ui-sweet-alert.html">Sweet-Alert</a></li>
-                        <li><a href="ui-tabs-accordions.html">Tabs & Accordions</a></li>
-                        <li><a href="ui-typography.html">Typography</a></li>
-                        <li><a href="ui-video.html">Video</a></li>
-                        <li><a href="ui-general.html">General</a></li>
-                        <li><a href="ui-colors.html">Colors</a></li>
-                        <li><a href="ui-rating.html">Rating</a></li>
-                        <li><a href="ui-notifications.html">Notifications</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="waves-effect">
-                        <i class="uil-shutter-alt"></i>
-                        <span class="badge rounded-pill bg-info float-end">9</span>
-                        <span>Forms</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="form-elements.html">Basic Elements</a></li>
-                        <li><a href="form-validation.html">Validation</a></li>
-                        <li><a href="form-advanced.html">Advanced Plugins</a></li>
-                        <li><a href="form-editors.html">Editors</a></li>
-                        <li><a href="form-uploads.html">File Upload</a></li>
-                        <li><a href="form-xeditable.html">Xeditable</a></li>
-                        <li><a href="form-repeater.html">Repeater</a></li>
-                        <li><a href="form-wizard.html">Wizard</a></li>
-                        <li><a href="form-mask.html">Mask</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="uil-list-ul"></i>
-                        <span>Tables</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="tables-basic.html">Bootstrap Basic</a></li>
-                        <li><a href="tables-datatable.html">Datatables</a></li>
-                        <li><a href="tables-responsive.html">Responsive</a></li>
-                        <li><a href="tables-editable.html">Editable</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="uil-chart"></i>
-                        <span>Charts</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="charts-apex.html">Apex</a></li>
-                        <li><a href="charts-chartjs.html">Chartjs</a></li>
-                        <li><a href="charts-flot.html">Flot</a></li>
-                        <li><a href="charts-knob.html">Jquery Knob</a></li>
-                        <li><a href="charts-sparkline.html">Sparkline</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="uil-streering"></i>
-                        <span>Icons</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="icons-unicons.html">Unicons</a></li>
-                        <li><a href="icons-boxicons.html">Boxicons</a></li>
-                        <li><a href="icons-materialdesign.html">Material Design</a></li>
-                        <li><a href="icons-dripicons.html">Dripicons</a></li>
-                        <li><a href="icons-fontawesome.html">Font Awesome</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="uil-location-point"></i>
-                        <span>Maps</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="maps-google.html">Google</a></li>
-                        <li><a href="maps-vector.html">Vector</a></li>
-                        <li><a href="maps-leaflet.html">Leaflet</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="uil-share-alt"></i>
-                        <span>Youth Club</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="true"> 
-                        <li><a href="javascript: void(0);" class="has-arrow">Discover</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="javascript: void(0);">Add New</a></li>
-                                <li><a href="javascript: void(0);">Manage</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
+                </li>  
 
             </ul>
         </div>
